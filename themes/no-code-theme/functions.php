92d6429bd30686a2f2e83d11e89f2d2de093cb0c
SAYED MAHMOOD SADAT <sayedmahmood.sadat@dcmail.ca>
2025-03-02 11:49:16 -0500

    Style Home page and enqueue style-css
    Style Home page and enqueue style-css are adedd

--- themes/no-code-theme/functions.php
+++ themes/no-code-theme/functions.php
@@ -12,7 +12,7 @@
 require_once get_theme_file_path( 'inc/hooks.php' );
 
 // Woocommerce.
-require_once get_theme_file_path( 'inc/woocommerce.php' );
+// require_once get_theme_file_path( 'inc/woocommerce.php' );
 
 
 function custom_enqueue_woocommerce_styles() {
@@ -23,3 +23,15 @@ function custom_enqueue_woocommerce_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'custom_enqueue_woocommerce_styles' );
+
+function no_code_theme_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css'); 
+}
+add_action('wp_enqueue_scripts', 'no_code_theme_enqueue_styles');
+
+function my_theme_enqueue_single_post_styles() {
+    if (is_single()) {  
+        wp_enqueue_style('single-post-styles', get_template_directory_uri() . '/assets/css/single-post.css');
+    }
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_single_post_styles');
