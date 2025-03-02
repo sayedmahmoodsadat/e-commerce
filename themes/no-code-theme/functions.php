9a308ed8d480c80a4df69cc632d4f51407b5bd0c
SAYED MAHMOOD SADAT <sayedmahmood.sadat@dcmail.ca>
2025-03-02 15:09:46 -0500

    Enqueue for the account.css and checkout
    Enqueue added for the account.css and checkout

--- themes/no-code-theme/functions.php
+++ themes/no-code-theme/functions.php
@@ -35,3 +35,48 @@ function my_theme_enqueue_single_post_styles() {
     }
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_single_post_styles');
+
+function enqueue_custom_login_styles() {
+    wp_enqueue_style('custom-login-styles', get_template_directory_uri() . '/assets/css/custom-login-styles.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_login_styles');
+
+
+function enqueue_custom_cart_styles() {
+    if (function_exists('is_cart') && is_cart()) {
+        wp_enqueue_style(
+            'custom-woocommerce-cart',
+            get_template_directory_uri() . '/assets/css/woocommerce-cart.css',
+            array(),
+            filemtime(get_template_directory() . '/assets/css/woocommerce-cart.css')
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_cart_styles');
+
+function enqueue_custom_checkout_styles() {
+    // Check if it's the checkout page
+    if (function_exists('is_checkout') && is_checkout()) {
+        // Enqueue the custom CSS file
+        wp_enqueue_style(
+            'custom-woocommerce-checkout', // Handle
+            get_template_directory_uri() . '/assets/css/woocommerce-checkout.css', // Path to CSS file
+            array(), // Dependencies
+            filemtime(get_template_directory() . '/assets/css/woocommerce-checkout.css') // Version (based on file modification time)
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_checkout_styles');
+
+
+function enqueue_custom_myaccount_styles() {
+    if (function_exists('is_account_page') && is_account_page()) {
+        wp_enqueue_style(
+            'custom-woocommerce-myaccount',
+            get_template_directory_uri() . '/assets/css/woocommerce-myaccount.css', // Path to your CSS file
+            array(),
+            filemtime(get_template_directory() . '/assets/css/woocommerce-myaccount.css') // Version based on file modification time
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_myaccount_styles');
